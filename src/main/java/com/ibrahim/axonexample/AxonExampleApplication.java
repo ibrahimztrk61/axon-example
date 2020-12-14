bfc1f5d105e5e8eb1d9c84afff20d353918f2986
ibrahimztrk61 <42765883+ibrahimztrk61@users.noreply.github.com>
2020-12-14 23:52:33 +0300

    Initial commit

---
+++ src/main/java/com/ibrahim/axonexample/AxonExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahim.axonexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxonExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibrahim/axonexample/AxonExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahim.axonexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
